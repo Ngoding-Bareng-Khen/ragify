4727b216422e1217c60e25ea049feadc966bff47
KhenCahyo13 <khencahyo02@gmail.com>
2026-06-03 20:23:50 +0700

    ci: enable and use pgvector

--- database/migrations/2026_05_31_193840_create_document_chunks_table.php
+++ database/migrations/2026_05_31_193840_create_document_chunks_table.php
@@ -11,12 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::ensureVectorExtensionExists();
+        
         Schema::create('document_chunks', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->foreignUuid('document_id')->constrained('documents')->cascadeOnDelete();
             $table->longText('content');
             $table->integer('chunk_index');
-            $table->json('embedding')->nullable();
+            $table->vector('embedding', dimensions: 1536)->index();
             $table->timestampsTz();
         });
     }
